f182206499c5cf2239d1453a8bf45dc932094cf9
Riley <pruittra@vcu.edu>
2025-09-15 11:00:09 -0400

    Changed commands to global

--- src/register-commands.js
+++ src/register-commands.js
@@ -29,8 +29,8 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
         // );
 
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands }
         );
 
         console.log('Successfully reloaded application (/) commands.');
